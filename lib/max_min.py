f9f1b5f93caccf6b5d2a2952a602c7ca61ced140
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-07 18:38:44 +0100

    Update max_min.py

--- lib/max_min.py
+++ lib/max_min.py
@@ -1,1 +1,14 @@
+import pandas as py
+import numpy as np
 
+results = pd.read_csv("Prem20232024Results.csv")
+home_score = results.HomeScore
+away_score = results.AwayScore
+max_home_score = np.max(home_score)
+max_away_score = np.max(away_score)
+min_home_score = np.min(home_score)
+min_away_score = np.min(away_score)
+print(max_home_score)
+print(max_away_score)
+print(min_home_score)
+print(min_away_score)
