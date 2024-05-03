5ee755ca83bb0f2b300e99c3655a0482decb78f3
plcr9 <paulcooperpc9@gmail.com>
2024-05-03 17:29:31 +0100

    high scores

--- lib/max_min.py
+++ lib/max_min.py
@@ -4,11 +4,18 @@ import numpy as np
 results = pd.read_csv("Prem20232024Results.csv")
 home_score = results.HomeScore
 away_score = results.AwayScore
+
 max_home_score = np.max(home_score)
 max_away_score = np.max(away_score)
 min_home_score = np.min(home_score)
 min_away_score = np.min(away_score)
+
 print(max_home_score)
 print(max_away_score)
 print(min_home_score)
 print(min_away_score)
+
+def highest_home_score_margin(max_home_score, min_away_score):
+    return np.diff((max_home_score, min_away_score))
+
+print(highest_home_score_margin(6, 1))
